7305774e9cf49cbdb7e1b5cbc94001131d92cec0
Aaron Curtis <foobarbecue@gmail.com>
2020-06-10 16:27:43 -0700

    Add blacklisting of nacs to find_nacs_mono

--- src/moon/nacpl/find_nacs_mono.py
+++ src/moon/nacpl/find_nacs_mono.py
@@ -2,7 +2,11 @@ from nacpl import find_stereo_pairs, geom_helpers
 from shapely import wkt
 import clize, json
 
-def bounding_box_mono(*, west, east, south, north):
+@clize.parser.value_converter
+def list_param(arg):
+    return arg.split(',')
+
+def bounding_box_mono(*, west:float, east:float, south:float, north:float, exclude: list_param=[]):
     """
     Like find_stereo_pairs.bounding_box but finds individual NACs rather than stereo pairs. Useful for creating image
     mosaics when not also computing stereo.
@@ -11,6 +15,7 @@ def bounding_box_mono(*, west, east, south, north):
     imgs = find_stereo_pairs.ImageSearch(
         polygon=wkt.dumps(search_poly_shapely)
     )
+    imgs.results = imgs.results.drop(exclude)
     imgs, stats = geom_helpers.covering_set_search(
         full_poly_set=imgs.results,
         search_poly=search_poly_shapely,
