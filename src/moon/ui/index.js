eb296882b76f8b2220617fa53b353e19a84c0f8c
Aaron Curtis <foobarbecue@gmail.com>
2020-07-23 15:42:06 -0700

    Correct parcel path and separate out js

--- src/moon/ui/index.js
+++ src/moon/ui/index.js
@@ -1,17 +1,37 @@
-import 'ol/ol.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+window.addEventListener('load', (event) => {
+    console.log('test')
+    document.getElementById('sendreq').onclick = submitWorkflowFromTemplate
+})
 
-const map = new Map({
-    target: 'map',
-    layers: [
-        new TileLayer({
-            source: new OSM()
-        })
-    ],
-    view: new View({
-        center: [0, 0],
-        zoom: 0
-    })
-});
+const submitWorkflow = (evt) => {
+    console.log(evt)
+}
+
+const submitWorkflowFromTemplate = () => {
+    fetch('/api/v1/workflow-templates/default/nac-stereo-wftmpl').then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        const frm = document.createElement('form')
+        document.body.appendChild(frm)
+        for (param of data.spec.arguments.parameters){
+            const inp = document.createElement('input', )
+            const lbl = document.createElement('label')
+            lbl.innerText = param.name
+            frm.appendChild(lbl)
+            frm.appendChild(inp)
+            frm.appendChild(document.createElement('p'))
+        }
+        const submit = document.createElement('input')
+        submit.type = 'submit'
+        submit.onclick = submitWorkflow
+        frm.appendChild(submit)
+
+    });
+}
+
+const start_wf = () => {
+    fetch('http://lmmp-pipeline-ubuntu:32019/api/v1/workflows/')
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+document.getElementById('sendreq').click = start_wf
