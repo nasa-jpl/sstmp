5ed6f01653e96226731a60e098b389a5af6cc836
Aaron Curtis <foobarbecue@gmail.com>
2020-07-24 16:53:15 -0700

    Leave mosaic footprint on map after request

--- src/moon/ui/index.js
+++ src/moon/ui/index.js
@@ -6,9 +6,11 @@ import {DragBox} from 'ol/interaction'
 import {defaults as defaultControls} from 'ol/control';
 import MousePosition from 'ol/control/MousePosition' 
 import {createStringXY} from 'ol/coordinate'
+import {Feature} from 'ol'
 
 let mosaicGoal
-const boxDrawLayer = new VectorLayer({source: new VectorSource({wrapX: false})})
+const boxDrawSource = new VectorSource({wrapX: false})
+const boxDrawLayer = new VectorLayer({source: boxDrawSource})
 const moonBaseMap = new TileLayer({
     source: new XYZ({
         url: 'https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png'
@@ -38,26 +40,37 @@ map.addControl(mousePositionControl)
 
 dragBox.on('boxend', (evt)=>{
     mosaicGoal = evt.target.getGeometry().clone().transform('EPSG:3857','EPSG:4326')
+    console.log(boxDrawSource)
     const mosaicExtent = mosaicGoal.getExtent()
     if (confirm(`Begin processing mosaic ${mosaicExtent.map((inp)=>inp.toPrecision(4))} ?`)){
         createMosaic(mosaicExtent)
+        boxDrawSource.addFeature(new Feature({geometry: evt.target.getGeometry()}))
     }
 })
 
 const createMosaic = (mosaicExtent) => {
+    const precision = 6
     // First, download the workflowtemplate for mosaics
     fetch('/api/v1/workflow-templates/default/nac-stereo-wftmpl').then(function (response) {
         return response.json();
     }).then((tmpl)=>{
         tmpl.spec.arguments.parameters = [
-            {name: 'west', value: mosaicExtent[0].toString()},
-            {name: 'south', value: mosaicExtent[1].toString()},
-            {name: 'east', value: mosaicExtent[2].toString()},
-            {name: 'north', value: mosaicExtent[3].toString()},
+            {name: 'west', value: mosaicExtent[0].toPrecision(precision)},
+            {name: 'south', value: mosaicExtent[1].toPrecision(precision)},
+            {name: 'east', value: mosaicExtent[2].toPrecision(precision)},
+            {name: 'north', value: mosaicExtent[3].toPrecision(precision)},
         ]
         submitMosaicWorkflow(tmpl)
     })
 }
+
+const updateMosaicWorkflowList = () => {
+    fetch('/api/v1/workflows/default')
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+window.updateMosaicWorkflowList = updateMosaicWorkflowList
 
 const submitMosaicWorkflow = (template) => {
     const workflowSpec = {
