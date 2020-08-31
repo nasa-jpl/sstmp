11ba2ae161709ac445579ac7a3824e5a88e9e148
Aaron Curtis <foobarbecue@gmail.com>
2020-08-30 21:56:09 -0700

    Shrink the footprints before covering set search to force overlap

--- src/moon/nacpl/find_nacs_mono.py
+++ src/moon/nacpl/find_nacs_mono.py
@@ -1,6 +1,6 @@
 from nacpl import find_stereo_pairs, geom_helpers
 from shapely import wkt
-import clize, json
+import clize, json, shapely
 
 @clize.parser.value_converter
 def list_param(arg):
@@ -17,10 +17,13 @@ def bounding_box_mono(*, west:float, east:float, south:float, north:float, exclu
     )
     imgs.results = imgs.results.drop(exclude)
     imgs.results['geometry'] = imgs.results.footprint_geometry
+    # Shrink all the footprints so that there will be overlap in final steps of mosaic creation
+    imgs.results['geometry'] = imgs.results.apply(lambda row: shapely.affinity.scale(row.geometry, 0.9, 0.9, 0.9), axis=1)
     imgs, stats = geom_helpers.covering_set_search(
         full_poly_set=imgs.results,
         search_poly=search_poly_shapely,
-        verbose=False
+        verbose=False,
+        plot=True
     )
     print(json.dumps(tuple(imgs.index.values)))
 
