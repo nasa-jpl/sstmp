d4f42aefd898a922631c6fbc3cb3bc8b42c2c27a
Aaron Curtis <foobarbecue@gmail.com>
2020-09-03 08:49:53 -0700

    Only change color on updates from the low-level templates

--- src/moon/ui/colors.js
+++ src/moon/ui/colors.js
@@ -1,11 +1,11 @@
 export const statusColors = {
     'select-nacs':     'rgba(207,196,196,0.4)',
     'find-pairs':      'rgba(255, 72,  0,0.4)',
-    'dl-ingest-nac':   'rgba(255,191,  0,0.4)',
-    'dl-ingest-left':  'rgba(187,255,  0,0.4)',
-    'dl-ingest-right': 'rgba(  0,255, 85,0.4)',
-    'download-nac':    'rgba(  0,255,225,0.4)',
-    'img2cub':         'rgba(  0, 94,255,0.4)',
+    // 'dl-ingest-nac':   'rgba(255,191,  0,0.4)',
+    // 'dl-ingest-left':  'rgba(255,191,  0,0.4)',
+    // 'dl-ingest-right': 'rgba(255,191,  0,0.4)',
+    'download-nac':    'rgba(129,183, 81,0.4)',
+    'img2cub':         'rgba( 85,148,135,0.4)',
     'calibrate':       'rgba(200,  0,255,0.4)',
     'highlighted':     'rgba(255,  0, 21,0.4)'
 } 
