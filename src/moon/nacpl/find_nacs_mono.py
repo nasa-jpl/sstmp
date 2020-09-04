dd707018a440c4bf941e50499b83e7739e0497bb
Aaron Curtis <foobarbecue@gmail.com>
2020-09-04 15:42:25 -0700

    feat: solar incidence filtering for mono mosaic

--- src/moon/nacpl/find_nacs_mono.py
+++ src/moon/nacpl/find_nacs_mono.py
@@ -6,6 +6,16 @@ import clize, json, shapely
 def list_param(arg):
     return arg.split(',')
 
+def filter_nacs_mono(img_set, min_incidence=30, max_incidence=65):
+    """
+    Culls NAC images before mono mosaic processing. This initial version only filters by solar incidence angle.
+    :param img_set: A GeoDataFrame of NAC imagery
+    :return: A GeoDataFrame of NAC imagery containing only suitable mosaic candidates
+    """
+    
+    filtered_img_set = img_set.loc[(img_set.incidence_angle > min_incidence) & (img_set.incidence_angle < max_incidence), :]
+    return filtered_img_set
+
 def bounding_box_mono(*, west:float, east:float, south:float, north:float, exclude: list_param=[]):
     """
     Like find_stereo_pairs.bounding_box but finds individual NACs rather than stereo pairs. Useful for creating image
@@ -17,6 +27,7 @@ def bounding_box_mono(*, west:float, east:float, south:float, north:float, exclu
     )
     # imgs.results = imgs.results.filter_sun_geometry()
     imgs.results = imgs.results.drop(exclude)
+    imgs.results = filter_nacs_mono(imgs.results)
     imgs.results['geometry'] = imgs.results.footprint_geometry
     # Shrink all the footprints so that there will be overlap in final steps of mosaic creation
     imgs.results['geometry'] = imgs.results.apply(lambda row: shapely.affinity.scale(row.geometry, 0.9, 0.9, 0.9), axis=1)
